ba5ca3dee1a909816a87031c3e3c6fec85d0a6f7
Sergei Chukhan <chuhan.sergey@gmail.com>
2016-10-09 01:55:27 +0300

    Fixed alphabet;

--- src/Generator.php
+++ src/Generator.php
@@ -9,7 +9,7 @@ class Generator {
      * Alphabet which will be used for generating new path
      * @var array
      */
-    protected $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    protected $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
     /**
